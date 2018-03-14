613b1b939b03489cbf3dc46104683f91e5859fe7
kostanzhoglo <jeffreywithers@gmail.com>
2018-03-14 15:34:05 -0400

    Add code to get '/topics' and topics/show.erb

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,7 +4,9 @@ class TopicsController < ApplicationController
     if !logged_in?
       redirect '/login'
     else
+      @users = User.all
       @user = current_user
+      @topics = Topic.all
       erb :'/topics/show'
     end
   end
