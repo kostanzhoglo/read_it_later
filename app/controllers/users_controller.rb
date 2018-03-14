0aa07309348d5ddf2935bf605ffbe69db2c558b8
kostanzhoglo <jeffreywithers@gmail.com>
2018-03-14 14:42:48 -0400

    Add post '/login' and get '/logout' to usersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,4 +25,23 @@ class UsersController < ApplicationController
     end
   end
 
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/topics'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    if !logged_in?
+      redirect '/'
+    else
+      session.clear
+      redirect '/login'
+    end
+  end
+
 end
