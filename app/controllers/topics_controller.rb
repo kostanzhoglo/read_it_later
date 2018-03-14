51a1ac9bc737f5091f5544a1a1e3641cd6babb9b
kostanzhoglo <jeffreywithers@gmail.com>
2018-03-14 12:38:14 -0400

    Add post '/signup' to UsersController and get '/topics' to TopicsController

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,12 @@
 class TopicsController < ApplicationController
+
+  get '/topics' do
+    if !logged_in
+      redirect '/login'
+    else
+      @user = current_user
+      erb :'/topics/show'
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,16 @@ class UsersController < ApplicationController
     erb :'/users/signup'
   end
 
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect '/signup'
+    else
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id
+      redirect '/topics'
+    end
+  end
+
   get '/login' do
     erb :'/users/login'
   end
