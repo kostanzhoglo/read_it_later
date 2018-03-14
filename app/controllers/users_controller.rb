c2b977a4d099bbaa28d8d8c3e66c06734f0ce9c7
kostanzhoglo <jeffreywithers@gmail.com>
2018-03-14 14:54:42 -0400

    Add Flash success & failure messages to signup, login, logout actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,12 @@ class UsersController < ApplicationController
 
   post '/signup' do
     if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      flash[:message] = "Error: Please don't leave any fields blank when signing up."
       redirect '/signup'
     else
       @user = User.create(username: params[:username], email: params[:email], password: params[:password])
       session[:user_id] = @user.id
+      flash[:message] = "Successfully signed in."
       redirect '/topics'
     end
   end
@@ -29,17 +31,21 @@ class UsersController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
+      flash[:message] = "Successfully logged in."
       redirect '/topics'
     else
+      flash[:message] = "Error: Something went wrong. Please enter your username and password again."
       redirect '/login'
     end
   end
 
   get '/logout' do
     if !logged_in?
+      flash[:message] = "You weren't logged in!"      
       redirect '/'
     else
       session.clear
+      flash[:message] = "Successfully logged out."
       redirect '/login'
     end
   end
