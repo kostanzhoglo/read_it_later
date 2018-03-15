0ada29b7bb4684ffb888b64e33cf99c349fd8462
kostanzhoglo <jeffreywithers@gmail.com>
2018-03-14 20:57:42 -0400

    Add #slug & .find_by_slug to Topic.rb & create show_single_topic.erb

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,13 @@ class Topic < ActiveRecord::Base
   has_many :articles
 
   validates_presence_of :name
+
+  def slug
+    name.downcase.gsub(/\W/, "-")
+  end
+
+  def self.find_by_slug(slug)
+    Topic.all.find {|topic| topic.slug == slug}
+  end
+
 end
