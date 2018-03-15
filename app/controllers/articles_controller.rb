cc5603ad5bd0fce900203f3b7643d56c9ef82923
kostanzhoglo <jeffreywithers@gmail.com>
2018-03-15 06:48:26 -0400

    Add use of rack-flash & get '/articles/new' to articlesController

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,15 @@
+require 'rack-flash'
+
 class ArticlesController < ApplicationController
+  use Rack::Flash
+
+  get '/articles/new' do
+    if !logged_in?
+      flash[:message] = "You have to be logged in to see that."
+      redirect '/login'
+    else
+      erb :'/articles/new'
+    end
+  end
+
 end
