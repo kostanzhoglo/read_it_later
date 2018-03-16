6d1afe899fc4132631f9745515769b864c1f4122
kostanzhoglo <jeffreywithers@gmail.com>
2018-03-16 11:50:54 -0400

    Add guard that article must be created with title & url or Redirect

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,30 +14,34 @@ class ArticlesController < ApplicationController
 
 
   post '/articles' do
-    @article = Article.create(title: params[:title], url: params[:url])
-    if !params[:name].empty?
-      @article.topic = Topic.find_or_create_by(name: params[:name])
-      @article.save
-      @topic = @article.topic
-      @topic.user = current_user
-      @topic.save
-      redirect "articles/#{@article.id}"
+    if params[:title] == "" || params[:url] == ""
+      flash[:message] = "Error. You can't leave Article Title or Web Address blank. Please try again."
+      redirect '/articles/new'
     else
-      @article.topic = Topic.find_by(id: params["topic_id"])
-      @article.save
-      @topic = @article.topic
-      @topic.user = current_user
-      @topic.save
-      redirect "articles/#{@article.id}"
+      @article = Article.create(title: params[:title], url: params[:url])
+      if !params[:name].empty?
+        @article.topic = Topic.find_or_create_by(name: params[:name])
+        @article.save
+        @topic = @article.topic
+        @topic.user = current_user
+        @topic.save
+        redirect "articles/#{@article.id}"
+      else
+        @article.topic = Topic.find_by(id: params["topic_id"])
+        @article.save
+        @topic = @article.topic
+        @topic.user = current_user
+        @topic.save
+        redirect "articles/#{@article.id}"
+      end
     end
   end
-
-
-
 
   get '/articles/:id' do
     @article = Article.find_by_id(params[:id])
     erb :'/articles/show'
   end
+
+
 
 end
