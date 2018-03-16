f71fdf9580e4c43c445f0ebfa630680b754a1495
kostanzhoglo <jeffreywithers@gmail.com>
2018-03-16 17:13:11 -0400

    Complete patch '/articles/:slug'. Bug still present in user validation portion of get '/articles/:slug/edit'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -73,34 +73,16 @@ class ArticlesController < ApplicationController
         @topic.user = current_user
         @topic.save
         redirect "/articles/#{@article.slug}"
+      else
+        @article.topic = Topic.find_by(id: params["topic_id"])
+        @article.save
+        @topic = @article.topic
+        @topic.user = current_user
+        @topic.save
+        redirect "/articles/#{@article.slug}"
       end
     end
   end
-
-  # patch '/articles/:slug' do
-    # if params[:title] == "" || params[:url] == ""
-    #   flash[:message] = "Error. You can't leave Article Title or Web Address blank. Please try again."
-    #   redirect "/articles/#{@article.slug}/edit"
-  #   else
-  #     @article = Article.find_by_slug(params[:slug])
-  #     @article = Article.update(title: params[:title], url: params[:url])
-      # if !params[:name].empty?
-      #   @article.topic = Topic.find_or_create_by(name: params[:name])
-      #   @article.save
-      #   @topic = @article.topic
-      #   @topic.user = current_user
-      #   @topic.save
-      #   redirect "/articles/#{@article.slug}"
-  #     else
-  #       @article.topic = Topic.find_by(id: params["topic_id"])
-  #       @article.save
-  #       @topic = @article.topic
-  #       @topic.user = current_user
-  #       @topic.save
-  #       redirect "/articles/#{@article.slug}"
-  #     end
-  #   end
-  # end
 
 
 end
