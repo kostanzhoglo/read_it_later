61f6293e578e61722922bbdadeb34ecdc6c89df5
kostanzhoglo <jeffreywithers@gmail.com>
2018-03-16 13:47:42 -0400

    Still debug slug for id substitution error, connected to looping

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,29 +25,30 @@ class ArticlesController < ApplicationController
         @topic = @article.topic
         @topic.user = current_user
         @topic.save
-        redirect "articles/#{@article.id}"
+        redirect "articles/#{@article.slug}"
       else
         @article.topic = Topic.find_by(id: params["topic_id"])
         @article.save
         @topic = @article.topic
         @topic.user = current_user
         @topic.save
-        redirect "articles/#{@article.id}"
+        redirect "articles/#{@article.slug}"
       end
     end
   end
 
-  get '/articles/:id' do
-    @article = Article.find_by_id(params[:id])
+  get '/articles/:slug' do
+    @article = Article.find_by_slug(params[:slug])
+    # binding.pry
     erb :'/articles/show'
   end
 
-  get '/articles/:id/edit' do
+  get '/articles/:slug/edit' do
     if !logged_in?
       flash[:message] = "You have to be logged in to see that."
       redirect '/login'
     else
-      @article = Article.find_by_id(params[:id])
+      @article = Article.find_by_slug(params[:slug])
       @topic = @article.topic
       if @article && @topic.user_id == current_user.id
         erb :'/articles/edit'
@@ -58,12 +59,12 @@ class ArticlesController < ApplicationController
     end
   end
 
-  patch '/articles/:id' do
-    # if params[:title] == "" || params[:url] == ""
-    #   flash[:message] = "Error. You can't leave Article Title or Web Address blank. Please try again."
-    #   redirect '/articles/:id/edit'
-    # else
-      # @article = Article.find_by_id(params[:id])
+  patch '/articles/:slug' do
+    if params[:title] == "" || params[:url] == ""
+      flash[:message] = "Error. You can't leave Article Title or Web Address blank. Please try again."
+      redirect "/articles/#{@article.slug}/edit"
+    else
+      @article = Article.find_by_slug(params[:slug])
       @article = Article.update(title: params[:title], url: params[:url])
       binding.pry
       if !params[:name].empty?
@@ -73,16 +74,16 @@ class ArticlesController < ApplicationController
         @topic = @article.topic
         @topic.user = current_user
         @topic.save
-        redirect "articles/#{@article.id}"
+        redirect "articles/#{@article.slug}"
       else
         @article.topic = Topic.find_by(id: params["topic_id"])
         @article.save
         @topic = @article.topic
         @topic.user = current_user
         @topic.save
-        redirect "articles/#{@article.id}"
+        redirect "articles/#{@article.slug}"
       end
-    # end
+    end
   end
 
 
